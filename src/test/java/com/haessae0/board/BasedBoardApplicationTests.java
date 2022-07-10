0695fe7bd5a60710261737edede11422208923df
haessae0 <haessae0@gmail.com>
2022-07-10 20:38:43 +0900

    #5 - started spring boot project
    initialized spring
    
    - spring web
    - spring actuator
    - dev tools
    - lombok

---
+++ src/test/java/com/haessae0/board/BasedBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haessae0.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasedBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
