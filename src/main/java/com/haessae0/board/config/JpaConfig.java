046e680d088c68c35fe5347deb9359a4cb3ebdd1
haessae0 <haessae0@gmail.com>
2022-07-11 20:36:10 +0900

    #7 - Spring Data JPA setting
    - added 'JpaConfig' for using JPA Auditing
    - some boot properties for DB and develop convenience
    - TODO : DB password leakage -> must change properties

---
+++ src/main/java/com/haessae0/board/config/JpaConfig.java
@@ -0,0 +1,18 @@
+package com.haessae0.board.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return () -> Optional.of("haessae0"); //시큐리티로 인증 기능 사용 시 수정하기
+    }
+}
